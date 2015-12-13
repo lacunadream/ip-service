f9af953c6089ad58ae7f1d2fa81e7b136d8afcb1
lacunadream <d.s.yong@lse.ac.uk>
2015-12-13 19:04:20 +0000

    initial set-up

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+ 
+ var app = express()
+// App shit
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res) {
+    res.json({ message: 'eff off' });   
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+});
