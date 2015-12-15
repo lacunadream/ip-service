1728789590ddc2b6b614cabbb2a2579334ca2bd6
lacunadream <d.s.yong@lse.ac.uk>
2015-12-15 02:58:44 +0000

    heroku ugh

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 var express = require('express');
-var app = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose')
+var fs = require('fs');
+ 
+ var app = express()
+// App shit
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
+app.post('/api', function(req, res) {
+	var ip = req.body.ip;
+	console.log(ip)
+	res.json(ip)
+})
 
 app.get('/', function(req, res) {
-  res.json({ message: 'eff off' });  
+    res.json({ message: 'eff off' });   
 });
 
 app.listen(app.get('port'), function() {
